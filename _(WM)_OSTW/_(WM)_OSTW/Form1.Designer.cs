7c020dd9e9cece8054f662f3d1b3283cbd5dfd7e
RGNGIT <68756004+RGNGIT@users.noreply.github.com>
2021-03-06 02:19:59 +0500

    Add files via upload

---
+++ _(WM)_OSTW/_(WM)_OSTW/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace __WM__OSTW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Awake = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.textBox15 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // Awake
+            // 
+            this.Awake.Location = new System.Drawing.Point(12, 327);
+            this.Awake.Name = "Awake";
+            this.Awake.Size = new System.Drawing.Size(590, 23);
+            this.Awake.TabIndex = 0;
+            this.Awake.Text = "Пуск";
+            this.Awake.UseVisualStyleBackColor = true;
+            this.Awake.Click += new System.EventHandler(this.Awake_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(552, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 47);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(552, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 86);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(552, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 125);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(552, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 164);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(552, 20);
+            this.textBox5.TabIndex = 10;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(570, 9);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(32, 20);
+            this.textBox6.TabIndex = 11;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(570, 47);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(32, 20);
+            this.textBox7.TabIndex = 12;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(570, 86);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(32, 20);
+            this.textBox8.TabIndex = 13;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(570, 125);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(32, 20);
+            this.textBox9.TabIndex = 14;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(570, 164);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(32, 20);
+            this.textBox10.TabIndex = 15;
+            // 
+            // textBox11
+            // 
+            this.textBox11.BackColor = System.Drawing.Color.Black;
+            this.textBox11.ForeColor = System.Drawing.Color.White;
+            this.textBox11.Location = new System.Drawing.Point(12, 283);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(70, 20);
+            this.textBox11.TabIndex = 16;
+            // 
+            // textBox12
+            // 
+            this.textBox12.BackColor = System.Drawing.Color.Black;
+            this.textBox12.ForeColor = System.Drawing.Color.White;
+            this.textBox12.Location = new System.Drawing.Point(143, 283);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(70, 20);
+            this.textBox12.TabIndex = 17;
+            // 
+            // textBox13
+            // 
+            this.textBox13.BackColor = System.Drawing.Color.Black;
+            this.textBox13.ForeColor = System.Drawing.Color.White;
+            this.textBox13.Location = new System.Drawing.Point(272, 283);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(70, 20);
+            this.textBox13.TabIndex = 18;
+            // 
+            // textBox14
+            // 
+            this.textBox14.BackColor = System.Drawing.Color.Black;
+            this.textBox14.ForeColor = System.Drawing.Color.White;
+            this.textBox14.Location = new System.Drawing.Point(401, 283);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.Size = new System.Drawing.Size(70, 20);
+            this.textBox14.TabIndex = 19;
+            // 
+            // textBox15
+            // 
+            this.textBox15.BackColor = System.Drawing.Color.Black;
+            this.textBox15.ForeColor = System.Drawing.Color.White;
+            this.textBox15.Location = new System.Drawing.Point(532, 283);
+            this.textBox15.Name = "textBox15";
+            this.textBox15.Size = new System.Drawing.Size(70, 20);
+            this.textBox15.TabIndex = 20;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 267);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "T0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(140, 267);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "T1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(269, 267);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "T2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(398, 267);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 13);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "T3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(529, 267);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 13);
+            this.label5.TabIndex = 25;
+            this.label5.Text = "T4";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 221);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(590, 23);
+            this.progressBar1.TabIndex = 26;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 362);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox15);
+            this.Controls.Add(this.textBox14);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Awake);
+            this.Name = "Form1";
+            this.Text = "_OSTW";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Awake;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.TextBox textBox15;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ _(WM)_OSTW/_(WM)_OSTW/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace __WM__OSTW {
+
+    delegate void _ThreadTask(int TID);
+
+    struct ThreadInfo {
+        public bool Done;
+        public int Time;
+        public string ThreadName;
+    }
+
+    public partial class Form1 : Form {
+
+        /// <Блок Первичная инициализация>
+
+        static Mutex tMutex = new Mutex();
+        static ThreadInfo[] tInfo = new ThreadInfo[5];
+
+        public Form1() {
+
+            InitializeComponent();
+
+            for (int i = 0; i < 5; i++) {
+                tInfo[i].Done = false;
+                tInfo[i].ThreadName = $"Поток {i + 1}: ";
+                Controls["textBox" + (i + 1)].Text = "Ожидание запуска...";
+            }
+
+        }
+
+        /// <\><Блок Первичная инициализация>
+
+        /// <Блок Таски однозадачных потоков>
+
+        /// <Блок Инициализация или конкатенация делегата>
+
+        _ThreadTask lTaskMngmt = tAction1; // Обязательно с int-овым параметром
+
+        // lTaskMngmt += Имя Метода; ...
+
+        void ThreadInit(object obj) {
+            this.lTaskMngmt(Convert.ToInt32(obj));
+        }
+
+        /// <\><Блок Инициализация или конкатенация делегата>
+
+        static void tAction1(int TID) {
+            tMutex.WaitOne(); // Запрос и ожидание мьютекса
+            Thread.Sleep(3000); tInfo[TID].Time--; // Тут типа чето делает
+            if (tInfo[TID].Time <= 0) {
+                tInfo[TID].Done = true;
+            }
+            tMutex.ReleaseMutex(); // Освободил мьютекс и вышел
+        }
+
+        /// <\><Блок Таски однозадачных потоков>
+
+        /// <Блок События>
+
+        int CalcPercent(int Times, int TID) {
+            return (int)(100f - Math.Floor((float)tInfo[TID].Time / (float)Times * 100f));
+        }
+
+        void Percentage(int Times, int TID) {
+            Controls["TextBox" + (TID + 6)].Text = $"{CalcPercent(Times, TID)}%";
+            progressBar1.Value = CalcPercent(Times, TID);
+        }
+
+        bool tDone() {
+            int Sum = 0;
+            for (int i = 0; i < 5; i++) {
+                Sum += Convert.ToInt32(tInfo[i].Done);
+            }
+            if (Sum == 5) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        private void Awake_Click(object sender, EventArgs e) {
+
+            int[] Times = new int[5];
+            for (int i = 0; i < 5; i++) {
+                tInfo[i].Time = Convert.ToInt32(Controls["textBox" + (i + 11)].Text); Times[i] = tInfo[i].Time;
+            }
+
+            while (true) {
+                for (int i = 0; i < 5; i++) {
+                    if (!tInfo[i].Done) {
+                        Thread tThread = new Thread(new ParameterizedThreadStart(ThreadInit));
+                        tThread.Start(i);
+                        Controls["textBox" + (i + 1)].Text = $"{tInfo[i].ThreadName}Выполняется. Запросил мьютекс.";
+                        tThread.Join();
+                        Controls["textBox" + (i + 1)].Text = $"{tInfo[i].ThreadName}Готов. Освободил мьютекс.";
+                    }
+                    if (tInfo[i].Done) {
+                        Controls["textBox" + (i + 1)].Text = $"Готов. Выполнился за {Times[i] * 3000f}ms";
+                    }
+                    Percentage(Times[i], i);
+                }
+                if (tDone()) { break; }
+            }
+        }
+
+        /// <\><Блок События>
+
+    }
+}
